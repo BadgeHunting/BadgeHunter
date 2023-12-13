80c7e17c2c0837979ba8fa3eb9e43e1ec4cd25cc
Edmar <jose.fortyfive@gmail.com>
2023-12-13 19:39:26 +0800

    Modified Account.java Co-authored-by: jose.fortyfive@gmail.com

--- Account.java
+++ Account.java
@@ -1,3 +1,24 @@
 public class Account {
 
+    private String name;
+    private String accountNumber;
+    private String email;
+
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accoutNumber){
+        this.accountNumber = accoutNumber;
+    }
 }
+
