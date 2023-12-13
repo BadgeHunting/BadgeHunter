9bf3b3718197ca458ec02f3b1ad133ce448c5d4c
Edmar <jose.fortyfive@gmail.com>
2023-12-13 19:26:55 +0800

    Modified Main.java Co-authored-by: jose.fortyfive@gmail.com

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ public class Main{
     for(int i = 0; i <= 10; i++){
       System.out.println("Hello World!");
     }
+
+    for(int j = 0; j <= 10; j++){
+      System.out.println("Hello World!");
+    }
       
   }
 }
